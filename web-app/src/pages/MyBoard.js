516dbb3e25d8d0e650605e0e0cf67412c7e62d5e
sam-gupta-git <sam78748@gmail.com>
2024-10-22 01:37:05 -0500

    Added item dragging

--- web-app/src/pages/MyBoard.js
+++ web-app/src/pages/MyBoard.js
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Draggable from 'react-draggable';
 import './MyBoard.css'; // We'll create this CSS file
 
 function MyBoard() {
+  const [items, setItems] = useState([
+    { id: 1, content: 'Sample Item 1', x: 100, y: 50 },
+    { id: 2, content: 'Sample Item 2', x: 300, y: 150 },
+  ]);
+
+  const handleDrag = (id, data) => {
+    setItems(prevItems =>
+      prevItems.map(item =>
+        item.id === id ? { ...item, x: data.x, y: data.y } : item
+      )
+    );
+  };
+
   return (
     <div className="my-board">
       <div className="pinboard">
-        <div className="pinned-item" style={{top: '50px', left: '100px'}}>
-          Sample Item 1
-        </div>
-        <div className="pinned-item" style={{top: '150px', left: '300px'}}>
-          Sample Item 2
-        </div>
-        {/* Add more pinned items as needed */}
+        {items.map(item => (
+          <Draggable
+            key={item.id}
+            defaultPosition={{x: item.x, y: item.y}}
+            onStop={(e, data) => handleDrag(item.id, data)}
+          >
+            <div className="pinned-item">
+              {item.content}
+            </div>
+          </Draggable>
+        ))}
       </div>
     </div>
   );
